f9ad55fc62eee76588b7f50263ad53fc42a446c6
Rodion <rodion.developer1@gmail.com>
2021-11-13 09:18:58 +0000

    Install laravel-ide-helper

--- config/app.php
+++ config/app.php
@@ -34,6 +34,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
