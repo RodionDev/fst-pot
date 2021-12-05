9b22e24f56aabcab9ebbdfb2d60e4df208bb0fe6
Rodion <rodion.developer1@gmail.com>
2021-12-05 11:51:14 +0000

    Add notification if view has no users, change blade structure

--- views/backend/admin/users/index.blade.php
+++ views/backend/admin/users/index.blade.php
@@ -4,7 +4,5 @@
     <h1>Benutzerliste</h1>
 @stop
 @section('content')
-@if ($users->isNotEmpty())
 @include('backend.admin.users._user_index_loop')
-@endif
 @stop
--- views/backend/admin/users/index_registrations.blade.php
+++ views/backend/admin/users/index_registrations.blade.php
@@ -4,7 +4,5 @@
     <h1>Registrierungen <small>Unverifizierte Nutzer</small></h1>
 @stop
 @section('content')
-@if ($users->isNotEmpty())
 @include('backend.admin.users._user_index_loop')
-@endif
 @stop
