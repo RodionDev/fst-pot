1b15b576483a8af412cc05ccd8f5183771a7d305
Rodion <rodion.developer1@gmail.com>
2022-04-23 21:56:33 +0000

    Add ServiceWorker, enable PWA

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,31 @@
 const mix = require('laravel-mix');
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+mix.webpackConfig({
+    plugins: [
+        new SWPrecacheWebpackPlugin({
+            cacheId: 'pwa',
+            filename: 'service-worker.js',
+            staticFileGlobs: ['public*.{css,eot,svg,png,jpg,ttf,woff,woff2,js,html}'],
+            minify: true,
+            stripPrefix: 'public/',
+            handleFetch: true,
+            dynamicUrlToDependencies: {
+                '/': ['views/frontend/pages/primary/front.blade.php'],
+                '/produkt': ['views/frontend/pages/primary/produkt.blade.php'],
+                '/datenschutz': ['views/frontend/pages/secondary/datenschutz.blade.php'],
+                '/impressum': ['views/frontend/pages/secondary/impressum.blade.php'],
+            },
+            staticFileGlobsIgnorePatterns: [/\.map$/, /mix-manifest\.json$/, /manifest\.json$/, /service-worker\.js$/],
+            navigateFallback: '/',
+            runtimeCaching: [
+                {
+                    urlPattern: /^https:\/\/fonts\.googleapis\.com\
+                    handler: 'cacheFirst'
+                }
+            ],
+        })
+    ]
+});
 mix.js('resources/js/app-frontend.js', 'public/js')
    .sass('resources/sass/frontend/app-frontend.scss', 'public/css');
 mix.sass('resources/sass/backend/app-backend.scss', 'public/css');
