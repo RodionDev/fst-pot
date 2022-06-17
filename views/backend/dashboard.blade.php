6b3bfd3dff9a9741dfd643a6e20c398c73024a06
Rodion <rodion.developer1@gmail.com>
2022-06-17 23:59:22 +0000

    Add user registration chart

--- views/backend/dashboard.blade.php
+++ views/backend/dashboard.blade.php
@@ -5,6 +5,7 @@
 @stop
 @section('content')
 <div class="row">
+    @include('backend.dashboard.infobox-user-chart')
     @include('backend.dashboard.infobox-messages')
     @include('backend.dashboard.infobox-quote')
 </div>
