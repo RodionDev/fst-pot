e9d896a2e9b7640ae6fbc68460d3b5ef76ebdc58
Rodion <rodion.developer1@gmail.com>
2022-08-31 09:39:27 +0000

    Style heading style

--- views/backend/admin/users/index_registrations.blade.php
+++ views/backend/admin/users/index_registrations.blade.php
@@ -1,7 +1,7 @@
 @extends('adminlte::page')
 @section('title', 'Benutzer')
 @section('content_header')
-    <h1>Registrierungen <small>Unverifizierte Nutzer</small></h1>
+    <h1>Registrierungen (unverifizierte Nutzer)</h1>
 @stop
 @section('content')
 @include('backend.admin.users._user_index_loop')
