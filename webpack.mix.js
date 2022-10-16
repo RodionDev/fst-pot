47ed3e3b7bdc99f56781f879b460b48f87a8b48f
Rodion <rodion.developer1@gmail.com>
2022-10-16 15:20:33 +0000

    Remove purgecss, update packages

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,18 +1,9 @@
 const mix = require('laravel-mix');
 const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
-require('laravel-mix-purgecss');
-$purgeOptions = {
-    folders: ['views','app','resources'],
-    whitelistPatterns: [/^view-/],
-    whitelist: ['vspot', 'skin-black'],
-};
 mix.js('resources/js/app-frontend.js', 'public/js')
-   .sass('resources/sass/frontend/app-frontend.scss', 'public/css')
-   .purgeCss($purgeOptions);
-mix.sass('resources/sass/backend/app-backend.scss', 'public/css')
-   .purgeCss($purgeOptions);
-mix.sass('resources/sass/web-access/web-access.scss', 'public/css')
-   .purgeCss($purgeOptions);
+   .sass('resources/sass/frontend/app-frontend.scss', 'public/css');
+mix.sass('resources/sass/backend/app-backend.scss', 'public/css');
+mix.sass('resources/sass/web-access/web-access.scss', 'public/css');
 mix.webpackConfig({
     plugins: [
         new SWPrecacheWebpackPlugin({
